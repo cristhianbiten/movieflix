c9b7f5f3a4048444c609f7d9784b3851d198d4ae
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-04-11 19:54:52 -0300

    Add user service

---
+++ src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -0,0 +1,49 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.UserDTO;
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.UserRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private AuthService authService;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = repository.findByEmail(s);
+        if (user == null) {
+            logger.error("User not found: " + s);
+            throw new UsernameNotFoundException("User not found");
+        }
+        logger.info("User found: " + s);
+
+        return user;
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO findById(Long id) {
+        authService.validateSelfOrAdmin(id);
+        Optional<User> obj = repository.findById(id);
+        User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not Found"));
+        return new UserDTO(entity);
+    }
+}
